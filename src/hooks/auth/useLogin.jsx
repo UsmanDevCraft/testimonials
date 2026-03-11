ef0d207f50242498c0614f8af14d3b9acfae8f91
Muhammad Usman Awan <uawan8154@gmail.com>
2026-03-11 15:19:14 +0500

    feat: Implement a global alert context and replace native browser alerts with custom UI notifications across the application.

--- src/hooks/auth/useLogin.jsx
+++ src/hooks/auth/useLogin.jsx
@@ -1,21 +1,26 @@
 import { useMutation } from "@tanstack/react-query";
 import { login } from "../../services/auth/auth.service";
 import { useNavigate } from "react-router-dom";
+import { useAlert } from "../../context/alertContext";
 
 export const useLogin = () => {
   const navigate = useNavigate();
+  const { showAlert } = useAlert();
 
   const mutation = useMutation({
     mutationFn: login,
     onSuccess: (data) => {
       console.log("Login successful!", data);
       localStorage.setItem("token", data.authToken);
-      alert("Login was Successfull, HAPPY HACKING!");
+      showAlert(
+        data.message || "Login was Successfull, HAPPY HACKING!",
+        "success",
+      );
       navigate("/home");
     },
     onError: (error) => {
-      alert(error.message);
       console.error(error.message);
+      showAlert(error.message || "Something went wrong", "error");
     },
   });
 
