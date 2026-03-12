d5070122ffb18202435e4d2023509c68a9381074
Muhammad Usman Awan <uawan8154@gmail.com>
2026-03-12 12:24:37 +0500

    feat: add auth based rendering in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-const Home = lazy(() => import("./pages/app/Home"));
-const SpacePage = lazy(() => import("./pages/app/SpacePage"));
-const Newspace = lazy(() => import("./components/Newspace"));
-const Addreview = lazy(() => import("./components/Addreview"));
 import "./styles/Home.css";
 import MainPage from "./pages/app/MainPage";
 import Login from "./pages/auth/Login";
@@ -11,6 +7,10 @@ import Signup from "./pages/auth/Signup";
 import ProtectedRoutes from "./utils/ProtectedRoutes";
 import Navbar from "./components/Navbar";
 import Loading from "./components/Loader";
+const Home = lazy(() => import("./pages/app/Home"));
+const SpacePage = lazy(() => import("./pages/app/SpacePage"));
+const Newspace = lazy(() => import("./components/Newspace"));
+const Addreview = lazy(() => import("./components/Addreview"));
 
 const App = () => {
   return (
